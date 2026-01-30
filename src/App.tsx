ee0d0c83c0ecc0bce191afa76dac7dbe385550e2
Denis Mahoney <denismahoney@Mac-mini.local>
2026-01-29 23:52:55 -0500

    Remove unused page imports from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.tsx';
-import ServiceLandingPage from './pages/ServiceLandingPage.tsx';
 import Header from './components/Header.tsx';
 import Footer from './components/Footer.tsx';
 
-import About from './pages/About.tsx';
-import ForContractors from './pages/ForContractors.tsx';
 import Privacy from './pages/Privacy.tsx';
 import Terms from './pages/Terms.tsx';
-import BlogIndex from './pages/BlogIndex.tsx';
-import BlogPost from './pages/BlogPost.tsx';
-import NotFound from './pages/NotFound.tsx';
-
-import GetQuote from './pages/GetQuote.tsx';
-import CostGuidePage from './pages/CostGuidePage.tsx';
 
 import ScrollToTop from './components/ScrollToTop.tsx';
 
@@ -26,16 +17,8 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/for-contractors" element={<ForContractors />} />
-            <Route path="/get-quote" element={<GetQuote />} />
-            <Route path="/blog" element={<BlogIndex />} />
-            <Route path="/blog/:slug" element={<BlogPost />} />
             <Route path="/privacy" element={<Privacy />} />
             <Route path="/terms" element={<Terms />} />
-            <Route path="/cost/:serviceSlug" element={<CostGuidePage />} />
-            <Route path="/:serviceSlug" element={<ServiceLandingPage />} />
-            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer />
